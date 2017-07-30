577150f53f6427b284d0fdd6b1ad64a3de6f0edb
yjv-XCV <khoo.jack@gmail.com>
2017-07-31 00:44:15 +0800

    Create Controller

---
+++ app/Http/Controllers/Office/OfficeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Office;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OfficeController extends Controller
+{
+    //
+    public function show()
+    {
+
+    }
+
+    public function createPage()
+    {
+
+    }
+
+    public function create ()
+    {
+
+    }
+
+    public function editPage()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function showArchieved()
+    {
+
+    }
+
+    public function archieve()
+    {
+    	
+    }
+}
---
+++ app/Http/Controllers/Visual/VisualController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Visual;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VisualController extends Controller
+{
+    //
+    public function console()
+    {
+
+    }
+
+    public function display()
+    {
+    	
+    }
+}
